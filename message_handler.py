018f940b9c609d05e48613a5b7c6879207d95057
wiktorpp <grazynapp65@gmail.com>
2022-07-09 13:55:15 +0200

    Added back the debug code with permissions requiremen

--- message_handler.py
+++ message_handler.py
@@ -77,3 +77,48 @@ async def on_message(message):
             open("dump.dat", "w").write(str(data))
             await self.dm("", file="dump.dat")
             exit()
+
+        if message.content.startswith("%"):
+            import io
+            import sys
+            import traceback
+            import textwrap
+            try:
+                old_stdout = sys.stdout
+                new_stdout = io.StringIO()
+                sys.stdout = new_stdout
+                request = message.content[1:]
+                
+                try:
+                    exec(
+                        "async def request_funct(other_locals):\n"
+                        "    locals().update(other_locals)\n"
+                        f"    return_value = ({request})\n"
+                        "    globals().update(locals())\n"
+                        "    return return_value\n"
+                        "globals().update(locals())\n"
+                    )
+                except:
+                    request = request.replace('\n', '\n    ')
+                    exec(
+                        "async def request_funct(other_locals):\n"
+                        "    locals().update(other_locals)\n"
+                        f"    {request}\n"
+                        "    globals().update(locals())\n"
+                        "    return return_value\n"
+                        "globals().update(locals())\n"
+                    )
+                return_value = await request_funct(locals())
+
+                output = new_stdout.getvalue()
+                sys.stdout = old_stdout
+
+                if return_value != None:
+                    output = str(return_value) + "\n" + output
+            except KeyError:
+
+                error = traceback.format_exc()
+                await message.channel.send(error)
+            else:
+                for chunk in textwrap.wrap(output, 2000, replace_whitespace=False):
+                    await message.channel.send(chunk)
